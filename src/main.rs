ff26f589bee4ccc7db6670b2504ea3db6b82676c
hit0tsu__ <Maltzev.alexandr@gmail.com>
2022-12-25 23:31:08 +0300

    Добавлена возможность для пользователя указать задержку перед запуском бота в секундах

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use enigo::*;
 use rand::*;
-use std::{println, thread, time};
+use std::{io, println, thread, time};
 
-static START_DELAY_VALUE: u64 = 15000;
 static FORWARD_BORDER: u64 = 200;
 
 fn generate_random_number() -> u64 {
@@ -17,12 +16,26 @@ fn generate_random_number() -> u64 {
 }
 
 fn main() {
-    println!("Запущен бот для фарма на koth, пожалуйста, откройте игру в течение 15 секунд");
+    //println!("Запущен бот для фарма на koth, пожалуйста, откройте игру в течение 15 секунд");
     let mut enigo = Enigo::new();
     let mut rotation_border_1: i32 = 0;
     let mut rotation_border_2: i32 = 0;
+    let mut start_delay_value = String::new();
 
-    let start_delay = time::Duration::from_millis(START_DELAY_VALUE);
+    println!("Введите задержку в секундах перед запуском бота, чтобы Вы успели открыть игру");
+
+    io::stdin()
+        .read_line(&mut start_delay_value)
+        .expect("Failed to read line");
+
+    let start_delay_value: u64 = start_delay_value.trim().parse().expect("Введите число!");
+
+    let start_delay = time::Duration::from_millis(start_delay_value * 1000);
+
+    println!(
+        "Установлена задержка перед запуском бота в размере {} секунд, пожалуйста, откройте игру в течение {} секунд",
+        &start_delay_value, &start_delay_value
+    );
 
     thread::sleep(start_delay);
     enigo.key_down(Key::Shift);
@@ -35,7 +48,6 @@ fn main() {
         let mut forward_step: u64 = generate_random_number(); // получаем рандомное число
         if rotation_border_1 < 300 {
             rotation_border_1 = rotation_border_1 + 1;
-
 
             // Так мы каждый раз бежим вперед разное количество времени
             forward_step = forward_step + 1;
